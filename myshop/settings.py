09657922ce112c70371da649c9f2efa785fc2ae9
AdrianDomin <adriandominn@gmail.com>
2018-03-14 18:43:54 +0100

    ver 0.0.3 - kategorie produktów, informacje o produkcie

--- myshop/settings.py
+++ myshop/settings.py
@@ -101,3 +101,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#multimedia
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+#koszyk
+CART_SESSION_ID ='cart'
--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.urlresolvers import reverse
+
 
 # Create your models here.
 
@@ -16,6 +18,11 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category',
+                       args=[self.slug])
 
 class Product(models.Model):
     category = models.ForeignKey(Category,
@@ -37,4 +44,9 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail',
+                       args=[self.id, self.slug])
 
